a3ef045de41df852cc33e3c07e592f02f0bb924e
DKBdg <strong.every@list.ru>
2023-03-19 16:15:35 +0600

    check

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Fight
-
-class FightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Fight
-        fields =('id','code','first_user', 'second_user', 'created_at')
--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import FightView
-
-urlpatterns = [
-    path('', FightView.as_view())
-]
